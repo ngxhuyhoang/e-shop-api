7daeb1c330fbcdaf23cdbce7e88ebd058886995d
Hoang Nguyen Huy <ngxhuyhoang@gmail.com>
2024-01-08 16:42:56 +0700

    Remove DevtoolsModule from app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { DevtoolsModule } from '@nestjs/devtools-integration';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from '~/databases/type-orm-config.service';
 import { AuthModule } from './modules/auth/auth.module';
@@ -14,9 +13,6 @@ import { ProductModule } from './modules/product/product.module';
     CacheModule.register({
       isGlobal: true,
       ttl: 1000 * 60, // 1 day
-    }),
-    DevtoolsModule.register({
-      http: process.env.NODE_ENV !== 'production',
     }),
     ConfigModule.forRoot({
       envFilePath: ['.env.development', '.env.production', '.env.staging'],
