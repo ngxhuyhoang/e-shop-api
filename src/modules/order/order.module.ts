a77c941f97780e7e7b7c68929c9368da9e562ed1
Hoang Nguyen Huy <ngxhuyhoang@gmail.com>
2024-01-16 14:49:21 +0700

    Add ProductEntity to imports in OrderModule

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -4,9 +4,12 @@ import { OrderController } from './order.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderEntity } from './entities/order.entity';
 import { OrderProductEntity } from './entities/order-product.entity';
+import { ProductEntity } from '../product/entities/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OrderEntity, OrderProductEntity])],
+  imports: [
+    TypeOrmModule.forFeature([OrderEntity, OrderProductEntity, ProductEntity]),
+  ],
   controllers: [OrderController],
   providers: [OrderService],
 })
